8184017fcf380db724dc91c4632396e23599aef6
Jose Cepero <ceperojose54@gmail.com>
2023-06-29 15:18:07 -0400

    added toggle completed handler

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -14,24 +14,39 @@ export default class App extends React.Component {
       todoNameInput: '',
     }
   }
+  setAxiosError = err => {this.setState({...this.state, error: err.response.data.message})
+}
   fetchAllTodos = ()=>{
     axios.get(URL)
     .then(res => {
       this.setState({...this.state, todos: res.data.data})
     })
-    .catch(err => {
-      this.setState({...this.state, error: err.response.data.message})
-    })
+    .catch(this.setAxiosError)
   }
   postNewTodo = (task) =>{
     axios.post(URL,{name: task})
-      .then(res => this.fetchAllTodos())
-      .catch(err =>{
-        this.setState({...this.state, error: err.response.data.message})
+      .then(res => {
+        this.setState({...this.state, todos: this.state.todos.concat(res.data.data)})
       })
+      .catch(
+        this.setAxiosError
+      )
+    }
 
+    toggleCompleted = id => () =>{
+      axios.patch(`${URL}/${id}`)
+      .then(res => {
+        console.log(res.data.data)
+        this.setState({
+          ...this.state, todos: this.state.todos.map(task => {
+          if (task.id !== id)
+            return task
+          return res.data.data
+        })})
+      })
+      .catch(this.setAxiosError)
 
-  }
+    }
   
   componentDidMount() {
     this.fetchAllTodos()
@@ -43,10 +58,11 @@ export default class App extends React.Component {
       All My Todos:
       {
         this.state.todos.map(task => {
-         return <p key={task.id}>{task.name}</p>
+         return <p onClick={this.toggleCompleted(task.id)} key={task.id}>{task.name} {task.completed? ' ✅':''}</p>
         })
       }
       <Form addItem={this.postNewTodo}/>
+      
       </>
     )
   }
