8b726fd8c649851251535012cd0bda61eb89239f
Jose Cepero <ceperojose54@gmail.com>
2023-06-29 15:58:19 -0400

    added functionality to clear button

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -12,6 +12,7 @@ export default class App extends React.Component {
       todos: [],
       error: '',
       todoNameInput: '',
+      displayCompleted: true,
     }
   }
   setAxiosError = err => {this.setState({...this.state, error: err.response.data.message})
@@ -51,18 +52,34 @@ export default class App extends React.Component {
   componentDidMount() {
     this.fetchAllTodos()
   }
+  toggleDisplayComleted = e =>{
+    e.preventDefault();
+    this.setState({...this.state, displayCompleted: !this.state.displayCompleted})
+  }
+
+  clearHandler = e =>{
+    e.preventDefault();
+   const openTasks = this.state.todos.filter(task =>{
+      return !task.completed
+    })
+    this.setState({...this.state, todos: openTasks})
+  }
   render() {
     return (
       <>
       <div id='errors'>Error: {this.state.error || 'none'}</div>
       All My Todos:
       {
-        this.state.todos.map(task => {
-         return <p onClick={this.toggleCompleted(task.id)} key={task.id}>{task.name} {task.completed? ' ✅':''}</p>
-        })
+        this.state.todos.reduce( (acc, task) => {
+         if(this.state.displayCompleted || !task.completed){
+          return acc.concat(<p onClick={this.toggleCompleted(task.id)} key={task.id}>{task.name} {task.completed? ' ✅':''}</p>)
+         }
+         return acc
+        }, [])
       }
+      {/* return <p onClick={this.toggleCompleted(task.id)} key={task.id}>{task.name} {task.completed? ' ✅':''}</p> */}
       <Form addItem={this.postNewTodo}/>
-      
+      <button onClick={this.toggleDisplayComleted}>{this.state.displayCompleted? 'Hide': 'Show'} Clompeted</button>
       </>
     )
   }
